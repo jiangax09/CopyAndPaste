714403f1b854bc560806e05211538a3e5ac083da
Alex Jiang <alex@nobody.com>
2025-10-01 01:04:34 -0700

    dot cfg

--- nn0/test.py
+++ nn0/test.py
@@ -1,33 +1,9 @@
 from graphviz import Digraph
 
-def trace(root):
-    nodes, edges = set(), set()
-    def dfs(node):
-        if node in nodes:
-            return
-        nodes.add(node)
-        for child in node._children:
-            edges.add((child, node))
-            dfs(child)
-    dfs(root)
-    return nodes, edges
+from value import Value
+from draw import draw_cfg
 
-def draw_dot(root, format='svg', rankdir='LR'):
-    """
-    format: png | svg | ...
-    rankdir: TB (top to bottom graph) | LR (left to right)
-    """
-    assert rankdir in ['LR', 'TB']
-    nodes, edges = trace(root)
-    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
-
-    for n in nodes:
-        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, n.grad), shape='record')
-        if n._op:
-            dot.node(name=str(id(n)) + n._op, label=n._op)
-            dot.edge(str(id(n)) + n._op, str(id(n)))
-
-    for n1, n2 in edges:
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-
-    return dot
+x = Value(1.0)
+y = (x * 2 + 1)
+y.backward()
+draw_cfg(y)
--- nn0/value.py
+++ nn0/value.py
@@ -1,7 +1,54 @@
 class Value:
-    def __init__(self, data, children=(), _op=''):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def __radd(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = _backward
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def backward(self):
+        visited = set()
+        topo = []
+
+        def build_topo(node):
+            if node not in visited:
+                visited.add(node)
+                for child in node._prev:
+                    build_topo(child)
+                topo.append(node)
+        build_topo(self)
+
+        self.grad = 1
+        for node in reversed(topo):
+            node._backward()
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
