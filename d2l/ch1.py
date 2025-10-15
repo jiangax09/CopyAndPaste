d1d2a962ff6b83ac7d3d37159faab03536dc052c
Alex Jiang <alex@nobody.com>
2025-10-14 21:28:03 -0700

    test

--- d2l/ch1.py
+++ d2l/ch1.py
@@ -1,7 +1,18 @@
 import torch
+import os
+import pandas
 
 
 # stride [::3, ::2]
 t0 = torch.tensor([[1, 2, 3, 4], [2, 1, 3, 4], [4, 3, 2, 1], [4, 3, 1, 2]])
 t1 = t0[::3, ::2]
 print(t1)
+
+os.makedirs(os.path.join('..', 'data'), exist_ok=True)
+data_file = os.path.join('..', 'data', 'house_tiny.csv')
+with open(data_file, 'w') as f:
+    f.write('NumRooms,Alley,Price\n')
+    f.write('NA,Pave,127500\n')
+    f.write('2,NA,106000\n')
+    f.write('4,NA,178100\n')
+    f.write('NA,NA,140000\n')
--- d2l/ch3.py
+++ d2l/ch3.py
@@ -31,27 +31,25 @@ def data_iter(batch_size, features, labels):
 
 batch_size = 10
 
-for X, y in data_iter(batch_size, features, labels):
-    print(X, '\n', y)
-    break
+#for X, y in data_iter(batch_size, features, labels):
+#    print(X, '\n', y)
+#    break
 
 def linreg(X, w, b):  #@save
-    """线性回归模型"""
     return torch.matmul(X, w) + b
 
 def squared_loss(y_hat, y):  #@save
-    """均方损失"""
+    # print(f"y.shape is {y.shape}, y_hat.shape is {y_hat.shape}")
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
 
 def sgd(params, lr, batch_size):  #@save
-    """小批量随机梯度下降"""
     with torch.no_grad():
         for param in params:
             param -= lr * param.grad / batch_size
             param.grad.zero_()
 
 lr = 0.03
-num_epochs = 300000000
+num_epochs = 10
 net = linreg
 loss = squared_loss
 w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
@@ -59,11 +57,12 @@ b = torch.zeros(1, requires_grad=True)
 
 for epoch in range(num_epochs):
     for X, y in data_iter(batch_size, features, labels):
-        l = loss(net(X, w, b), y)  # X和y的小批量损失
-        # 因为l形状是(batch_size,1)，而不是一个标量。l中的所有元素被加到一起，
-        # 并以此计算关于[w,b]的梯度
+        l = loss(net(X, w, b), y)
         l.sum().backward()
-        sgd([w, b], lr, batch_size)  # 使用参数的梯度更新参数
+        sgd([w, b], lr, batch_size)
     with torch.no_grad():
         train_l = loss(net(features, w, b), labels)
         print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+
+print(f"w = {w}")
+print(f"b = {b}")
